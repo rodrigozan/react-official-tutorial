242822580d07611c184074207f4adf966bccc999
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2021-11-17 12:00:39 -0300

    Set the state of game with xIsNext

--- src/index.js
+++ src/index.js
@@ -2,29 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
-class Square extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      value: null
-    }
-  }
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button
+      className="square"
+      onClick={props.onClick}
+    >
+      {props.value}
+    </button>
+  )
 }
 
 class Board extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { squares: Array(9).fill(null) }
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true
+    }
   }
 
   handleClick(i) {
